49d647ae4c1193317494f0e721d945d93e486586
Tajah Johnson <tajah93@hotmail.com>
2020-12-05 13:53:01 -0600

    Added icons and icon buttons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { MDBIcon, MDBBtn} from 'mdbreact';
+import "./Header.css"
 
 function Header () {
     return (
-        <div>
-            <h1>U-Room</h1>
+        <div className= "header">
+            <MDBBtn floating size="lg" gradient="blue" className= "icons"><MDBIcon icon="graduation-cap" className= "logo" /></MDBBtn>
+            <MDBBtn floating size="lg" gradient="blue" className= "icons"><MDBIcon far icon="user" /></MDBBtn>
+            <MDBBtn floating size="lg" gradient="blue" className= "icons"><MDBIcon far icon="comments" /></MDBBtn>
+           
         </div>
     );
 }
