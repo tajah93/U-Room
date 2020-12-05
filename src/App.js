3cc86322b2646e866de673c1a1036414c2399fbe
Tajah Johnson <tajah93@hotmail.com>
2020-12-05 13:54:19 -0600

    Added and imported routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import './App.css';
 import Header from './components/Header/Header'; 
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom"; 
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Router> 
       <header className="App-header">
-        {}
-        <Header />
-        </header>
+       
+       
+        
+        <Switch>
+          
+          <Route path= "/message">
+          </Route>
+          {/* homepage route always at the bottom */}
+          <Route path= "/">
+          </Route>
+        </Switch>
+
         
 
+       
+        </header>
+        
+        </Router>
     </div>
   );
 }
