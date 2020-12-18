ba57807bab68406dc318b9899f3065c88404502d
Tajah Johnson <tajah93@hotmail.com>
2020-12-18 00:12:22 -0600

    Changed schema name

--- models/roommies.js
+++ models/roommies.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const schema = new Schema({
+const roomSchema = new Schema({
     name: { type: String, required: true },
     url: [{type: String, required: true}]
   });
 
-  const roommies = mongoose.model("roommies", schema);
+  const roommies = mongoose.model("roommies", roomSchema);
   
   module.exports = roommies;
