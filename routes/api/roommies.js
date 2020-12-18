166d77c9cbc0fb2692400e8d6c74a6bf631b608c
Tajah Johnson <tajah93@hotmail.com>
2020-12-18 00:11:35 -0600

    Created get and post routes from controller

---
+++ routes/api/roommies.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const controllers = require("../../controllers/controllers");
+
+
+router.route("/")
+  .get(controllers.findAll)
+  .post(controllers.create);
+
+
+router.route("/:id")
+  .get(controllers.findById)
+  .put(controllers.update)
+  .delete(controllers.remove);
+
+module.exports = router;
