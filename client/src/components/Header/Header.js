39a2f7c482081c952a8e40643ee2e491a2692bef
Tajah Johnson <tajah93@hotmail.com>
2020-12-19 20:12:19 -0600

    Fixed a few bugs

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,9 +5,9 @@ import "./Header.css"
 function Header () {
     return (
         <div className= "header">
-            <MDBBtn floating size="lg"  className= "icons" color= "white" idname= "gradIcon"><MDBIcon icon="graduation-cap" className= "logo" /></MDBBtn>
-            <MDBBtn floating size="lg" color= "white"  className= "icons"><MDBIcon far icon="user" /></MDBBtn>
-            <MDBBtn floating size="lg" color= "white" className= "icons"><MDBIcon far icon="comments" /></MDBBtn>
+            <MDBBtn className= "icons" color= "white" idname= "gradIcon"><MDBIcon icon="graduation-cap" className= "logo" /></MDBBtn>
+            <MDBBtn  color= "white"  className= "icons"><MDBIcon far icon="user" /></MDBBtn>
+            <MDBBtn  color= "white" className= "icons"><MDBIcon far icon="comments" /></MDBBtn>
            
         </div>
     );
