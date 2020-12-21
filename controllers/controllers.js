d22f16ffb7be04c3907b93d8ddd66b099b110354
Tajah Johnson <tajah93@hotmail.com>
2020-12-21 16:38:30 -0600

    Fixed 500 error with controlers

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -10,6 +10,12 @@ module.exports = {
             .catch(err =>
                 res.status(422).json(err));
     },
+    create: function(req, res) {
+      db.roommies
+        .create(req.body)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
     findById: function(req, res) {
       db.roommies
         .findById(req.params.id)
